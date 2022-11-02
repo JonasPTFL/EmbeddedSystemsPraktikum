489bcf9e67742115940b97769d54100c610ef5de
Jonas <js.pp@gmx.de>
2022-11-02 11:43:00 +0100

    adds working functions to main that allow led control

--- src/main.c
+++ src/main.c
@@ -1,4 +1,41 @@
+typedef unsigned int uint32_t;
 
-int main(){
-    return 0;
+#define REG(P) (*(volatile uint32_t *) (P))
+
+#define GPIO_BASE 0x10012000
+#define GPIO_INPUT_EN 0x4
+#define GPIO_OUTPUT_EN 0x8
+#define GPIO_OUTPUT_VAL 0xc
+#define GPIO_IOF_EN 0x38
+
+#define BLUE_LED 5
+#define GREEN_LED 18
+
+
+void setup_led(int gpio_pin){
+	REG(GPIO_BASE + GPIO_IOF_EN) &= ~(1 << gpio_pin);
+	REG(GPIO_BASE + GPIO_INPUT_EN) &= ~(1 << gpio_pin);
+	REG(GPIO_BASE + GPIO_OUTPUT_EN) |= 1 << gpio_pin;
+	REG(GPIO_BASE + GPIO_OUTPUT_VAL) |= (1 << gpio_pin);
+}
+
+void enable_led(int led, int state){
+    if (state){
+		REG(GPIO_BASE + GPIO_OUTPUT_VAL) |= (1 << led);
+    } else{
+	    REG(GPIO_BASE + GPIO_OUTPUT_VAL) &= ~(1 << led);
+    }
+    
+}
+
+int main (void)
+{
+	// setup LEDs
+    setup_led(BLUE_LED);
+    setup_led(GREEN_LED);
+
+	// toggle only blue LED
+    enable_led(BLUE_LED, 1);
+    enable_led(GREEN_LED, 1);
+
 }
