b971b900dce7287dbaa816b44c58eb14fdfe2104
Jonas <js.pp@gmx.de>
2022-11-02 11:56:57 +0100

    adds first button implementiation

--- src/main.c
+++ src/main.c
@@ -7,16 +7,30 @@ typedef unsigned int uint32_t;
 #define GPIO_OUTPUT_EN 0x8
 #define GPIO_OUTPUT_VAL 0xc
 #define GPIO_IOF_EN 0x38
+#define GPIO_INPUT_VAL 0x0
+#define GPIO_PUE 0x10
 
 #define BLUE_LED 5
 #define GREEN_LED 18
+#define BUTTON 19
 
+typedef enum {
+    FALSE, TRUE
+} boolean;
 
 void setup_led(int gpio_pin){
 	REG(GPIO_BASE + GPIO_IOF_EN) &= ~(1 << gpio_pin);
 	REG(GPIO_BASE + GPIO_INPUT_EN) &= ~(1 << gpio_pin);
 	REG(GPIO_BASE + GPIO_OUTPUT_EN) |= 1 << gpio_pin;
 	REG(GPIO_BASE + GPIO_OUTPUT_VAL) |= (1 << gpio_pin);
+}
+
+void setup_button(int gpio_pin){
+	REG(GPIO_BASE + GPIO_IOF_EN) &= ~(1 << gpio_pin);
+	REG(GPIO_BASE + GPIO_PUE) |= 1 << gpio_pin;
+	REG(GPIO_BASE + GPIO_INPUT_EN) |= 1 << gpio_pin;
+	REG(GPIO_BASE + GPIO_OUTPUT_EN) &= ~(1 << gpio_pin);
+	REG(GPIO_BASE + GPIO_OUTPUT_VAL) &= ~(1 << gpio_pin);
 }
 
 void enable_led(int led, int state){
@@ -28,14 +42,30 @@ void enable_led(int led, int state){
     
 }
 
+boolean is_pressed(int button){
+    return REG(GPIO_BASE + GPIO_INPUT_VAL) & (1 << BUTTON); 
+}
+
 int main (void)
 {
 	// setup LEDs
     setup_led(BLUE_LED);
     setup_led(GREEN_LED);
 
+    setup_button(BUTTON);
+
 	// toggle only blue LED
-    enable_led(BLUE_LED, 1);
-    enable_led(GREEN_LED, 1);
+    enable_led(BLUE_LED, 0);
+    enable_led(GREEN_LED, 0);
+
+    while(1){
+		if (is_pressed(BUTTON)){
+            enable_led(BLUE_LED, 1);
+            enable_led(GREEN_LED, 1);
+		} else {
+            enable_led(BLUE_LED, 0);
+            enable_led(GREEN_LED, 0);
+        }
+    }
 
 }
