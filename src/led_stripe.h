ead8893f7cdbfb8390d7bd41e02dbdde68f5f7a0
Jonas Pollpeter <js.pp@gmx.de>
2022-12-21 23:02:23 +0100

    first idea of assembly procedure

--- src/led_stripe.h
+++ src/led_stripe.h
@@ -14,25 +14,26 @@ void setup_led_stripe(void){
 	REG(GPIO_BASE + GPIO_OUTPUT_VAL) |= (1U << LED_STRIPE);
 }
 
-static void enable_led_stripe(uint_t *color_data, uint_t data_length)
+static void enable_led_stripe(uint_t *color_data)
 {
     uint_t * current_colors = color_data;
-    uint_t current_length = data_length;
     uint_t pin = (1U << LED_STRIPE);
     uint_t reg = REG(GPIO_BASE + GPIO_OUTPUT_VAL);
 
 
     asm volatile(
-                "li t0, 10\n"
-                "loadbyte:\n"
-                    "lb t1, 0(%[current_colors])\n"
-                "beginloop:\n"
+                "li t0, 10\n" // iterator counter
+                "byteloop:\n"
                     "beqz t0, endloop\n"
-                    "beqz t1, writelow\n"
+                    "li t1, 0x80\n" // load a binary mask to filter only relevant bit
+                    "lb t2, (%[current_colors])\n" // load byte in t2
+                "bitloop:\n"
+                    "and  t3, t1, t2\n" // apply mask and store mask result in t3
+                    "srli t1, t1, 1\n" // right shift mask to check next bit
+                    "sw %[high], (%[output])\n" // begin first high output
+                    "beqz t2, writelow\n" // jump to low procedure if bit is 0
                  "writehigh:\n" // write high output
-                    "sw %[high], (%[output])\n"
-                    "nop\n"
-                    "nop\n"
+                    "addi %[current_colors], %[current_colors], 1\n" // increase pointer to next elem // TODO low high proceduren mit zeiten anpassen, sonst funktionsfähig?!
                     "nop\n"
                     "nop\n"
                     "nop\n"
@@ -41,7 +42,6 @@ static void enable_led_stripe(uint_t *color_data, uint_t data_length)
                     "nop\n"
                     "nop\n"
                     "sw %[low], (%[output])\n"
-                    "nop\n"
                     "nop\n"
                     "addi t0, t0, -1\n"
                     "j beginloop\n"
@@ -61,10 +61,10 @@ static void enable_led_stripe(uint_t *color_data, uint_t data_length)
                     "nop\n"
                     "addi t0, t0, -1\n"
                     "j beginloop\n"
-                // "endloop:\n"
-                //  "end:\n"
-                //  "sw   %[high], 0(%[output])\n"
-                //  "sw   %[low],  0(%[output])\n" // reset
+                "endloop:\n"
+                 "end:\n"
+                 "sw   %[high], 0(%[output])\n"
+                 "sw   %[low],  0(%[output])\n" // reset
                  :
                  : [output] "r" (GPIO_CTRL_ADDR + GPIO_OUTPUT_VAL),
                    [high]   "r" (reg |  pin),
@@ -88,7 +88,7 @@ void show_next_led_stripe_colors(void){
 
     static color colors[] = { blue, red, green, blue, red, green, blue, red, green, blue };
     color *current_colors = colors + color_index;
-    enable_led_stripe((uint_t *)current_colors, 10);
+    enable_led_stripe((uint_t *)current_colors);
     
     color_index++;
     
