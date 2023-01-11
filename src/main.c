591d1d62d51d6e418301c787d5f7f3ee3e895cd6
Jonas Pollpeter <js.pp@gmx.de>
2023-01-11 12:21:31 +0100

    working oled code, but only when debug with sparkfun redboard

--- src/main.c
+++ src/main.c
@@ -4,6 +4,13 @@
 #include "startup.h"
 #include "wrap.h"
 #include "types.h"
+/* includes for oled display */
+#include "encoding.h"
+#include "platform.h"
+#include "display.h"
+#include "font.h"
+
+#include <string.h>
 
 
 
@@ -16,10 +23,12 @@ void irq_handler() __attribute__((interrupt));
 void vTask1( void *pvParameters );
 void vTask2( void *pvParameters );
 void vTask3( void *pvParameters );
+void vTask4( void *pvParameters );
 
 const char *pcTextForTask1 = "Enabled Green LED\n";
 const char *pcTextForTask2 = "Enable Blue LED\n";
 const char *pcTextForTask3 = "Enable Yellow LED\n";
+const char *pcTextForTask4 = "Hallo printed\n";
 
 /*-----------------------------------------------------------*/
 int main( void )
@@ -32,12 +41,22 @@ int main( void )
 	xTaskCreate( vTask1, "Enabled Green LED", 1000, (void*)pcTextForTask1, 2, NULL );
 	xTaskCreate( vTask2, "Enable Blue LED", 1000, (void*)pcTextForTask2, 1, NULL );
 	xTaskCreate( vTask3, "Enable Yellow LED", 1000, (void*)pcTextForTask3, 1, NULL );
+	xTaskCreate( vTask4, "Print Hallo", 1000, (void*)pcTextForTask4, 2, NULL );
 
 	/* start scheduler */
 	vTaskStartScheduler();
 
 	for( ;; ){}
 	return 0;
+}
+
+/* delay the program for a specific amount of milliseconds  */
+void delay(uint32_t milliseconds){
+    const volatile uint64_t *now = (volatile uint64_t*)(CLINT_CTRL_ADDR + (uint64_t)CLINT_MTIME);
+    volatile uint64_t then = *now + (uint64_t)milliseconds*((uint64_t)RTC_FREQ / (uint64_t)MILLISECONDS_PER_SECOND);
+    while ((*now) < then){
+        
+    }
 }
 
 /* enables all leds with the given state  */
@@ -51,13 +70,17 @@ void enabled_all_leds(boolean state){
 /* enables a pecific led with the given state  */
 void enable_led(uint32_t led, boolean state){
     if (state == TRUE){
-		  REG(GPIO_BASE + GPIO_OUTPUT_VAL) |= (1U << led);
+		REG(GPIO_BASE + GPIO_OUTPUT_VAL) |= (1U << led);
     } else{
 	    REG(GPIO_BASE + GPIO_OUTPUT_VAL) &= ~(1U << led);
     }
 }
 
 void setup(void){
+	oled_init();
+	
+	delay(2000);
+	
 	setup_led(GREEN_LED);
 	setup_led(BLUE_LED);
 	setup_led(YELLOW_LED);
@@ -135,6 +158,13 @@ void irq_handler()
 	// toggle led
 	REG(GPIO_BASE + GPIO_OUTPUT_VAL) ^= (1 << RED_LED);
 
+
+	printChar('T');
+	printChar('e');
+	printChar('s');
+	printChar('t');
+	printChar('1');
+
 	// clear gpio pending interrupt
 	REG(GPIO_BASE + GPIO_RISE_IP) |= (1 << BUTTON_LEFT_UP);
 
@@ -178,5 +208,28 @@ void vTask3( void *pvParameters )
 	for( ;; )
 	{
 		printf(pcTextForTask3);
+	}
+}
+
+/*-----------------------------------------------------------*/
+void vTask4( void *pvParameters )
+{
+
+	// const char* text = "Oled example...";
+	// uint32_t textpos = 0;
+	// while (textpos < strlen(text))
+	// {
+	// 	printChar(text[textpos]);
+	// 	if(textpos % ((DISP_W / CHAR_W) * (DISP_H/8)) == ((DISP_W / CHAR_W) * (DISP_H/8))-1)
+	// 	{
+	// 		delay(200);
+	// 	}
+	// 	delay(1000);
+	// 	textpos++;
+	// }
+
+	for( ;; )
+	{
+		printf(pcTextForTask4);
 	}
 }
